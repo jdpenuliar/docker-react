b324d8f0b33d30c7995532c4aeda3eb7ff39fe62
jdpenuliar <jaydz.penuliar@gmail.com>
2020-08-27 00:41:55 -0700

    change haha

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <button onClick={() => {
           setCount(count => count + 1);
-        }}>haha</button>
+        }}>haha JD was here?</button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
